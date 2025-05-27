9644406fcc199c1f47d0b4293a2f7ae664924c3d
suraj-xd <unofficialartist3@gmail.com>
2025-05-27 20:07:10 +0530

    hydration errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${satoshi.variable} ${instrumentSerif.variable}`}
     >
       <body>
--- src/components/homepage/projects/projects.tsx
+++ src/components/homepage/projects/projects.tsx
@@ -4,18 +4,17 @@ import { CardsThreeIcon } from "@phosphor-icons/react";
 import { useState } from "react";
 import { projectsData } from "~/constants/projects";
 import ProjectTooltip from "./project-tooltip";
-import StarFourBadge from "~/components/star-four-badge";
 
 export default function Projects() {
   const [hoveredProject, setHoveredProject] = useState<number | null>(null);
 
   return (
     <div className="flex w-full flex-col items-start justify-start">
-      <StarFourBadge
-        title="latest projects"
-        icon={<CardsThreeIcon size={12} />}
-      />
-
+      <div className="flex items-center justify-start gap-1 rounded-2xl rounded-b-none bg-gray-100 px-4 py-2 font-serif text-sm leading-tight text-black">
+        <CardsThreeIcon size={12} />
+        latest projects
+      </div>
+      
       <div className="flex items-center justify-start gap-1 rounded-full rounded-t-none">
         {projectsData.map((project, index) => (
           <ProjectTooltip key={project.id} project={project}>
